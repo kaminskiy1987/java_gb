dc26e2c66d033124de1732aa333c878887e14b02
Mikhail <m.kaminskiy@list.ru>
2023-01-05 17:06:12 +0700

    HomeWork3_OOP

--- HomeWork3_OOP/Controller.java
+++ HomeWork3_OOP/Controller.java
@@ -11,25 +11,25 @@ public class Controller {
         /**
          * Различные форматы
          */
-        String pathJson = "W1.json";
-        String pathMd = "W2.md";
-        String pathXml = "W3.xml";
+        String pathJson = "WorkerJson.json";
+        String pathMd = "WorkerMd.md";
+        String pathXml = "WorkerXml.xml";
         /**
          * Запись данных в различные форматы
          */
         Worker worker = new Worker(32, 35000, "Ivan", "Ivanov" );
-        Document doc = new Document<Worker>(worker);
+        Document document = new Document<Worker>(worker);
 
-        doc.setFormat(new JsonFormat());
-        doc.setPath(pathJson);
-        doc.saveInFormat();
+        document.setFormat(new JsonFormat());
+        document.setPath(pathJson);
+        document.saveInFormat();
 
-        doc.setFormat(new MdFormat());
-        doc.setPath(pathMd);
-        doc.saveInFormat();
+        document.setFormat(new MdFormat());
+        document.setPath(pathMd);
+        document.saveInFormat();
 
-        doc.setFormat(new XmlFormat());
-        doc.setPath(pathXml);
-        doc.saveInFormat();
+        document.setFormat(new XmlFormat());
+        document.setPath(pathXml);
+        document.saveInFormat();
     }
 }
